30dea1ee6f5ac854edf4a217d37fe57f3036e402
crvftsmvn <bitruss@gmail.com>
2025-11-10 22:33:02 -0500

    Update: ensure Render compatibility with PORT env variable

--- app.py
+++ app.py
@@ -602,4 +602,5 @@ def team_top_players():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
